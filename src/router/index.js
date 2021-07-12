235c1c85b77b29889220297eae33fb10e28d71ca
wangliang <340392404@qq.com>
2021-07-12 17:09:18 +0800

    routes 提取

--- src/router/index.js
+++ src/router/index.js
@@ -4,21 +4,21 @@ import About from '@/views/about'
 import Home from '@/views/home'
 import Blog from '@/views/blog'
 Vue.use(VueRouter)
-
+const routes = [
+    {
+      path: '/',
+      component:Home
+    },
+    {
+      path: '/about',
+      component:About
+    },
+    {
+      path: '/blog',
+      component:Blog
+    }
+  ]
 const router = new VueRouter({
-    routes: [
-      {
-        path: '/',
-        component:Home
-      },
-      {
-        path: '/about',
-        component:About
-      },
-      {
-        path: '/blog',
-        component:Blog
-      }
-    ]
+    routes
   })
   export default router
